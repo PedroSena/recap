82ac70307a49ebd1b5cf4ddd650b7f691b2d9f53
Pedro Sena <sena.pedro@gmail.com>
2014-10-25 11:58:35 -0300

    Displaying results

--- background.js
+++ background.js
@@ -1,27 +1,29 @@
 var db = openDatabase('recap','0.0.1', 'recap', 2 * 1024 * 1024);
 db.transaction(function(tx){
+	tx.executeSql("DRPO TABLE HISTORY");
 	tx.executeSql("CREATE TABLE IF NOT EXISTS HISTORY (url, content, date)");
 });
 
 chrome.extension.onMessage.addListener(function(message, sender, sendResponse){
 	if (message.type == "insert") {
 		db.transaction(function(tx){
+			var cleanText = removeHTML(message.content);
 			tx.executeSql("INSERT INTO HISTORY (url,content,date) VALUES (?,?,?)", 
-				[message.url, message.content, new Date().getTime()]);	
-			console.log("Inserted new entry on history");
+				[message.url, cleanText, new Date().getTime()]);
+			console.log(cleanText);
 		});	
 	}
 	if (message.type == "query") {
 		db.transaction(function(tx){
-			tx.executeSql("SELECT * FROM HISTORY", [], function(tx,result){
+			tx.executeSql("SELECT DISTINCT url,content FROM HISTORY where content like ?", ["%" + message.content + "%"], function(tx,result){
 				var len = result.rows.length;
+				console.log("# of entries for " + message.content + ": " + len);
 				var entries = [];
 				for ( var i = 0; i < len; i++ ) {
 					var item = result.rows.item(i);
 					var entry = {
 						url: item.url,
-						content: item.content,
-						date: item.date
+						content: item.content
 					};
 					entries.push(entry);
 				}
@@ -31,3 +33,9 @@ chrome.extension.onMessage.addListener(function(message, sender, sendResponse){
 	}
 	return true;
 });
+
+function removeHTML(content) {
+	content = content.replace(/<script.*>[\s\S]+?<\/script>/gi,"");
+	content = content.replace(/(\s){2,}/g,"");
+	return $(content).text();
+}
--- recap.js
+++ recap.js
@@ -7,13 +7,34 @@ window.onload = function(){
 		};
 		chrome.extension.sendMessage(message, function(results){
 			var entries = results.entries;
-			for ( var i = 0; i < 5; i++ ) {
+			$('.resultsContainer').empty();
+			for ( var i = 0; i < Math.min(5,entries.length); i++ ) {
 				var entry = entries[i];
 				var template = $('#resultTemplate').clone().show();
-				template.find('.url').html(entry.url);
-				template.find('.content').html("Hey");
+				template.find('.content')
+					.attr('src', entry.url)
+					.text(getRelevantContent(entry.content, text));
 				$('.resultsContainer').append(template);
 			}
 		});
 	}, false);
+
+	function getRelevantContent(allContent, keyword) {
+		var tokens = allContent.split(" ");
+		var indices = [];
+		for ( var i = 0; i < tokens.length; i++ ) {
+			var token = tokens[i];
+			if ( token == keyword ) {
+				indices.push(i);
+			}
+		}
+		var words = [];
+		for ( var i = 0; i < indices.length; i++ ) {
+			var index = indices[i];
+			for ( var j = -5; j < 6; j++ ) {
+				words.push(tokens[index+j]);	
+			}
+		}
+		return words.join(" ");
+	}
 }
