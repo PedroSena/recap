a1671a83f3adc4a06ed7c26ad2374982707ab966
Pedro Sena <sena.pedro@gmail.com>
2014-10-28 18:11:21 -0300

    Small template tweaks suggested by Jhonny

--- recap.js
+++ recap.js
@@ -5,15 +5,21 @@ window.onload = function(){
 			type: 'query',
 			content: text
 		};
+		if ( text.length < 3 ) return;
 		chrome.extension.sendMessage(message, function(results){
 			var entries = results.entries;
 			$('.resultsContainer').empty();
 			for ( var i = 0, limit = entries.length; i < limit; i++ ) {
 				var entry = entries[i];
 				var template = $('#resultTemplate').clone().show();
+				template.find('.content').attr('href', entry.url);
+				template.find('.title').text("..." + entry.content + "...");
+				template.find('.link').text(entry.url);
+				/*
 				template.find('.content')
 					.attr('href', entry.url)
 					.text("..." + entry.content + "... | " + entry.url);	
+				*/	
 				$('.resultsContainer').append(template);
 			}
 		});
